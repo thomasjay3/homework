0d6a5e728d09ad4903c88b3990f1cc0382b29b4e
Thomas <you@example.com>
2022-01-23 17:06:11 -0800

    update

---
+++ 10codons.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+# Print out all the codons for the sequence below in reading frame 1
+# Use a 'for' loop
+
+dna = 'ATAGCGAATATCTCTCATGAGAGGGAA'
+
+# print(dna)
+
+
+
+for i in range(len(dna)):
+	print(dna[i:i[3]]
+
+
+
+# your code goes here
+
+"""
+python3 10codons.py
+ATA
+GCG
+AAT
+ATC
+TCT
+CAT
+GAG
+AGG
+GAA
+"""
