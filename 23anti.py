000911197f2bb0a076b53cabf7a15487c8569fb7
Thomas <you@example.com>
2022-01-30 13:21:33 -0800

    submitted

---
+++ 23anti.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+# Write a program that prints the reverse-complement of a DNA sequence
+# You must use a loop and conditional
+
+dna = 'ACTGAAAAAAAAAAA'
+complement = ''
+
+for i in range(len(dna)+1):
+	if dna[-i] == 'A':
+		complement += 'T'
+	elif dna[-i] == 'T':
+		complement += 'A'
+	elif dna[-i] == 'C':
+		complement += 'G'
+	else:
+		complement += 'C'
+
+print(complement)
+
+
+
+"""
+python3 23anti.py
+TTTTTTTTTTTCAGT
+"""
